6275817c37171da621c79923faed2ba124b34229
snnamani <xtrahyptek@gmail.com>
2023-07-12 13:29:40 -0500

    terraform job 2nd commit

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
- required_providers {
-   aws = {                      # provider local name
-     source  = "hashicorp/aws"  # global and unique source address
-     version = "~> 4.0"         # version constrain
-   } 
- }
+  required_providers {
+    aws = {                     # provider local name
+      source  = "hashicorp/aws" # global and unique source address
+      version = "~> 4.0"        # version constrain
+    }
+  }
 }
 
 provider "aws" {
-    region = var.region
-  
+  region = var.region
+
 }
 
 resource "aws_iam_user" "devops" {
-  name = each.value
+  name     = each.value
   for_each = toset(var.iam-user)
   #path = "/system/"
   force_destroy = var.force_destroy
@@ -25,7 +25,7 @@ resource "aws_iam_user" "devops" {
 }
 #this need to be revisited
 #resource "aws_iam_access_key" "devops-key" {
- # user = aws_iam_user.devops.name
+# user = aws_iam_user.devops.name
 #}
 
 resource "aws_iam_policy" "poweruser" {
@@ -35,26 +35,26 @@ resource "aws_iam_policy" "poweruser" {
 }
 
 resource "aws_iam_user_policy_attachment" "policy-attach" {
-  for_each    = toset(var.iam-user)
-  user        = aws_iam_user.devops[each.key].name
-  policy_arn  = aws_iam_policy.poweruser.arn
+  for_each   = toset(var.iam-user)
+  user       = aws_iam_user.devops[each.key].name
+  policy_arn = aws_iam_policy.poweruser.arn
 }
 
 variable "region" {
-    type = string
-    description = "My prefered aws region"
-    default = "us-east-1"
-  
+  type        = string
+  description = "My prefered aws region"
+  default     = "us-east-1"
+
 }
 variable "force_destroy" {
-    type = bool
-    description = "deletion policy"
-    default = true
-  
+  type        = bool
+  description = "deletion policy"
+  default     = true
+
 }
 #for_each implementation
 variable "iam-user" {
-    type = list(string)
-    default = [ "chidiogo" ]
-  
+  type    = list(string)
+  default = ["chidiogo"]
+
 }
